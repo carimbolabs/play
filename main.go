8c575b7ed4c20cb467c5476f1b622701796d561c
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-11-14 16:20:10 -0300

    Work in progress

--- main.go
+++ main.go
@@ -259,7 +259,7 @@ func (w *gzipResponseWriter) Write(b []byte) (int, error) {
 
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		isIndexHTML := strings.HasSuffix(r.URL.Path, "index.html")
+		isIndexHTML := strings.HasSuffix(r.URL.Path, "/")
 
 		w.Header().Set("Content-Encoding", "gzip")
 		w.Header().Set("Vary", "Accept-Encoding")
