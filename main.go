661806991f225be23f692592800103ef71bf7e0d
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-11-14 17:48:53 -0300

    Work in progress

--- main.go
+++ main.go
@@ -280,7 +280,7 @@ func Middleware(next http.Handler) http.Handler {
 func main() {
 	server := &http.Server{
 		Addr: fmt.Sprintf(":%s", os.Getenv("PORT")),
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		Handler: Middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			path := r.URL.Path
 
 			switch {
@@ -295,7 +295,7 @@ func main() {
 			default:
 				rootHandler(w, r)
 			}
-		}),
+		})),
 	}
 
 	log.Fatal(server.ListenAndServe())
